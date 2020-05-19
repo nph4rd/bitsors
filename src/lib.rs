786bd7a757a41cd50bd884927e12a50eb3acd6aa
Arturo Marquez <arturomf94@gmail.com>
2020-05-18 22:54:07 -0500

    Use reqwest

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,14 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+    use std::collections::HashMap;
+    #[tokio::test]
+    async fn main() -> Result<(), Box<dyn std::error::Error>> {
+        let resp = reqwest::get("https://httpbin.org/ip")
+            .await?
+            .json::<HashMap<String, String>>()
+            .await?;
+        println!("{:#?}", resp);
+        Ok(())
+    }
 }
