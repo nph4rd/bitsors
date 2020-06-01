88fca6977f75374736ce8a64dca5457b71bdcbdd
arturomf94 <arturomf94@gmail.com>
2020-06-01 11:07:06 -0500

    Add errors (#5)
    * Add errors and refactor functions
    
    * Add models for available books and ticker. Add tests.
    
    * Use regular error
    
    * Send bad request message
    
    * Add order book model
    
    * Add model for trades

---
+++ src/model/public.rs
@@ -0,0 +1,82 @@
+/// From: https://bitso.com/api_info#available-books
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct AvailableBooks {
+    pub success: bool,
+    pub payload: Vec<AvailableBook>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct AvailableBook {
+    book: String,
+    minimum_amount: String,
+    maximum_amount: String,
+    minimum_price: String,
+    maximum_price: String,
+    minimum_value: String,
+    maximum_value: String,
+}
+
+/// From: https://bitso.com/api_info#ticker
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Ticker {
+    pub success: bool,
+    pub payload: BookTicker,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct BookTicker {
+    book: String,
+    volume: String,
+    high: String,
+    last: String,
+    low: String,
+    vwap: String,
+    ask: String,
+    bid: String,
+    created_at: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct OrderBook {
+    pub success: bool,
+    pub payload: OrderBookPayload,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct OrderBookPayload {
+    asks: Vec<Ask>,
+    bids: Vec<Bid>,
+    updated_at: String,
+    sequence: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Ask {
+    book: String,
+    price: String,
+    amount: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Bid {
+    book: String,
+    price: String,
+    amount: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Trades {
+    pub success: bool,
+    pub payload: Vec<Trade>,
+}
+
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Trade {
+    book: String,
+    created_at: String,
+    amount: String,
+    maker_side: String,
+    price: String,
+    tid: u64,
+}
--- tests/test_client.rs
+++ tests/test_client.rs
@@ -13,28 +13,54 @@ async fn test_available_books() {
 
 /// Test successful request to get ticker
 #[tokio::test]
-async fn test_ticker() {
+async fn test_ticker_successfull() {
     let bitso = Bitso::default()
         .build();
-    let result = bitso.get_ticker().await;
+    let result = bitso.get_ticker(Some("btc_mxn")).await;
+    println!("{:?}", result);
+}
+
+/// Test unsuccessful request to get ticker
+#[tokio::test]
+async fn test_ticker_unsuccessfull() {
+    let bitso = Bitso::default()
+        .build();
+    let result = bitso.get_ticker(Some("CREATEERROR")).await;
     println!("{:?}", result);
 }
 
 /// Test successful request to get order book
 #[tokio::test]
-async fn test_order_book() {
+async fn test_order_book_successfull() {
     let bitso = Bitso::default()
         .build();
     let result = bitso.get_order_book(Some("btc_mxn")).await;
     println!("{:?}", result);
 }
 
+/// Test unsuccessful request to get order book
+#[tokio::test]
+async fn test_order_book_unsuccessfull() {
+    let bitso = Bitso::default()
+        .build();
+    let result = bitso.get_order_book(Some("CREATEERROR")).await;
+    println!("{:?}", result);
+}
+
 /// Test successful request to get trades
 #[tokio::test]
-async fn test_trades() {
+async fn test_trades_successfull() {
     let bitso = Bitso::default()
         .build();
     let result = bitso.get_trades(Some("btc_mxn")).await;
     println!("{:?}", result);
 }
 
+/// Test unsuccessful request to get trades
+#[tokio::test]
+async fn test_trades_unsuccessfull() {
+    let bitso = Bitso::default()
+        .build();
+    let result = bitso.get_trades(Some("CREATEERROR")).await;
+    println!("{:?}", result);
+}
