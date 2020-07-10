2d2e71c23e74fec3f945beac67b868878bd29821
Arturo Marquez <arturomf94@gmail.com>
2020-07-09 22:48:52 -0500

    Use correct AccountStatus struct

--- src/model/private.rs
+++ src/model/private.rs
@@ -2,20 +2,25 @@
 /// From: https://bitso.com/api_info#account-status
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct AccountStatus {
-    pub client_id: String,
-    pub firts_name: String,
-    pub last_name: String,
-    pub status: String,
-    pub daily_limit: String,
-    pub daily_remaining: String,
-    pub monthly_limit: String,
-    pub monthly_remaining: String,
-    pub cash_deposit_allowance: String,
-    pub cellphone_number: String,
-    pub cellphone_number_stored: String,
-    pub email_stored: String,
-    pub official_id: String,
-    pub proof_of_residency: String,
-    pub signed_contract: String,
-    pub origin_of_funds: String,
+    pub success: bool,
+    pub payload: AccountStatusPayload,
+}
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct AccountStatusPayload {
+    client_id: String,
+    first_name: String,
+    last_name: String,
+    status: String,
+    daily_limit: String,
+    daily_remaining: String,
+    monthly_limit: String,
+    monthly_remaining: String,
+    cash_deposit_allowance: String,
+    cellphone_number: String,
+    cellphone_number_stored: String,
+    email_stored: String,
+    official_id: String,
+    proof_of_residency: String,
+    signed_contract: String,
+    origin_of_funds: String,
 }
