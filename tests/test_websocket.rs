36f071ccc68f0a7f056d8b9b551775ecb8328ecc
Bruno A. Muciño <mucinoab@gmail.com>
2021-03-27 19:35:40 -0600

    Initial WebSocket implementation. (#25)
    * Initial WebSocket implementation.
    
    * Fix clippy warning in tests.

---
+++ tests/test_websocket.rs
@@ -0,0 +1,43 @@
+use bitsors::websocket::*;
+
+#[cfg(test)]
+#[derive(serde_derive::Deserialize)]
+pub struct BitsoBooks {
+    pub payload: Vec<Book>,
+}
+
+#[cfg(test)]
+#[derive(serde_derive::Deserialize)]
+pub struct Book {
+    #[serde(rename = "book")]
+    pub name: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn all_books_and_proper_name() {
+        let current_books = reqwest::get("https://api.bitso.com/v3/available_books/")
+            .await
+            .unwrap()
+            .json::<BitsoBooks>()
+            .await
+            .unwrap();
+
+        assert_eq!(current_books.payload.len(), Books::COUNT);
+
+        let mut current_books: Vec<&str> = current_books
+            .payload
+            .iter()
+            .map(|b| b.name.as_str())
+            .collect();
+        current_books.sort_unstable();
+
+        let mut enum_books: Vec<String> = Books::iter().map(|b| b.to_string()).collect();
+        enum_books.sort_unstable();
+
+        assert_eq!(current_books, enum_books);
+    }
+}
