c9154b2af456ad0b97fbb8a5b44aa2c793e4ba1b
Arturo Marquez <arturomf94@gmail.com>
2021-04-02 17:40:45 -0600

    Change test for websocket

--- tests/test_websocket.rs
+++ tests/test_websocket.rs
@@ -13,31 +13,26 @@ pub struct Book {
     pub name: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[tokio::test]
+async fn all_books_and_proper_name() {
+    let current_books = reqwest::get("https://api.bitso.com/v3/available_books/")
+        .await
+        .unwrap()
+        .json::<BitsoBooks>()
+        .await
+        .unwrap();
 
-    #[tokio::test]
-    async fn all_books_and_proper_name() {
-        let current_books = reqwest::get("https://api.bitso.com/v3/available_books/")
-            .await
-            .unwrap()
-            .json::<BitsoBooks>()
-            .await
-            .unwrap();
+    assert_eq!(current_books.payload.len(), Books::COUNT);
 
-        assert_eq!(current_books.payload.len(), Books::COUNT);
+    let mut current_books: Vec<&str> = current_books
+        .payload
+        .iter()
+        .map(|b| b.name.as_str())
+        .collect();
+    current_books.sort_unstable();
 
-        let mut current_books: Vec<&str> = current_books
-            .payload
-            .iter()
-            .map(|b| b.name.as_str())
-            .collect();
-        current_books.sort_unstable();
+    let mut enum_books: Vec<String> = Books::iter().map(|b| b.to_string()).collect();
+    enum_books.sort_unstable();
 
-        let mut enum_books: Vec<String> = Books::iter().map(|b| b.to_string()).collect();
-        enum_books.sort_unstable();
-
-        assert_eq!(current_books, enum_books);
-    }
+    assert_eq!(current_books, enum_books);
 }
